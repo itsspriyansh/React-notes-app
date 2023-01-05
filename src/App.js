dd4869b45d13b72c3e4b51c4a52b634a3a281548
itsspriyansh <priyanshkp01@gmail.com>
2023-01-05 18:03:37 +0530

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid"
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 function App() {
+
+  const [searching, setSearching] = useState (false)
+  const [searchItem, setSearchItem] = useState ({})
+  const [item, setItem] = useState ([
+    {
+      id : nanoid(),
+      text : "hello there,\nI am priyansh, a developer, designer and a problem solver.",
+      date : "3/1/23"
+    },
+    {
+      id : nanoid(),
+      text : "This is a practice notes app created by me",
+      date : "3/1/23"   
+    },
+    {
+      id : nanoid(),
+      text : "my github id is @itsspriyansh",
+      date : "3/1/23"
+    }, 
+    {
+      id : nanoid(),
+      text : "Thank You\n : -)",
+      date : "3/1/23"
+    }
+  ])
+
+
+  function deleteHandler (id) {
+    let newNotes = []
+    item.map (note => {
+      if (note.id !== id) {
+        newNotes.push (note)
+      }
+    })
+    setItem (newNotes)
+  }
+
+  function saveChange (text) {
+
+    const date = new Date()
+    const note = {
+      id : nanoid(),
+      text : text,
+      date : date.toLocaleDateString(),
+    }
+    const newNotes = [...item, note]
+    setItem (newNotes)
+  }
+
+  function searchHandler (input) {
+    let newNotes = []
+    item.map (note => {
+      if (note.text.includes(input)) {
+        newNotes.push(note)
+        console.log (note)
+      }
+    })
+    setSearchItem (newNotes)
+  }
+
+  function searchState (input) {
+    setSearching(input)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search searchHandler={searchHandler} searchState={searchState} />
+      <NotesList notes={searching ? searchItem : item} saveChange={saveChange} deleteHandler={deleteHandler} />
     </div>
   );
 }
 
 export default App;
+
